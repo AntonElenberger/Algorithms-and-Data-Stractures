c936cdac42aa1664772b2f0bcfb57aa250fdc9fb
AntonElenberger <antonelenberger@gmail.com>
2018-10-04 21:06:30 +0500

    Homework2
    Added delete methods, and also added method sortBubbleWhithFlag.

---
+++ homework2/MainClass.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.antonelenberger.homework2;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Array array = new Array(5,9,4,3,7,3,8);
+//        array.delete(2);
+//        array.deleteAll(3);
+//        array.deleteAll();
+//        array.sortBubbleWithFlag();
+        System.out.println(array.toString());
+    }
+}
